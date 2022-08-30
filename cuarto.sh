792351397db4bb7b2bc698575e60e9d2a55a83d6
raul <raulgf1982@gmail.com>
2022-08-30 22:41:28 +0200

    remates

--- cuarto.sh
+++ cuarto.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Hola mundo desde BASH cuarto by rgfDev"
-
+open -a calculator.app
 #recibo arg desde java y pongo nombre a la copia del fichero
 cp /Users/raul/Dropbox/programacion/scriptingJava/primero.sh /Users/raul/Dropbox/programacion/scriptingJava/$1.sh
 echo "_______FIN DE SECUENCIA/SCRIPTING_______"
